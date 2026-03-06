e8493c47442c5430bcf25fe6e3b6c77902096e23
Prince Kumar <pkumar052@rku.ac.in>
2026-03-06 16:53:58 +0530

    Initial project structure and requirements configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Prince",
+    author_email="pkumar052@rku.ac.in",
+    packages=find_packages(),
+    install_requires=[],
+)
