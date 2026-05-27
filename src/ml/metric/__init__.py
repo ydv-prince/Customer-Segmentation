b263aac9a5a280ea2e9d1214cce5d16f34d9f2a9
Prince Kumar <pkumar052@rku.ac.in>
2026-05-27 14:20:12 +0530

    Add classification metrics and CustomerSegmentationModel S3 estimator

---
+++ src/ml/metric/__init__.py
@@ -0,0 +1,28 @@
+from sklearn.metrics import (confusion_matrix, f1_score, precision_score,
+                             recall_score)
+
+from src.entity.artifact_entity import ClassificationMetricArtifact
+
+
+def calculate_metric(model, x, y) -> ClassificationMetricArtifact:
+    """
+    model: estimator
+    x: input feature
+    y: output feature
+    """
+    yhat = model.predict(x)
+    classification_metric = ClassificationMetricArtifact(
+        f1_score=f1_score(y, yhat, average='weighted'),
+        recall_score=recall_score(y, yhat, average='weighted'),
+        precision_score=precision_score(y, yhat, average='weighted'),
+    )
+    return classification_metric
+
+
+def total_cost(y_true, y_pred):
+    """
+    This function takes y_ture, y_predicted, and prints Total cost due to misclassification
+    """
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+    cost = 10 * fp + 500 * fn
+    return cost
---
+++ src/ml/model/s3_estimator.py
@@ -0,0 +1,82 @@
+from src.cloud_storage.aws_storage import SimpleStorageService
+from src.exception import CustomerException
+from src.ml.model.estimator import CustomerSegmentationModel
+import sys
+from pandas import DataFrame
+
+
+
+class CustomerClusterEstimator:
+    """
+    This class is used to save and retrieve src model in s3 bucket and to do prediction
+    """
+
+    def __init__(self,bucket_name,model_path,):
+        """
+        :param bucket_name: Name of your model bucket
+        :param model_path: Location of your model in bucket
+        """
+        self.bucket_name = bucket_name
+        self.s3 = SimpleStorageService()
+        self.model_path = model_path
+        self.loaded_model:CustomerSegmentationModel=None
+
+
+    def is_model_present(self,model_path):
+        try:
+            return self.s3.s3_key_path_available(bucket_name=self.bucket_name, s3_key=model_path)
+        except Exception as e:
+            print(e)
+            return False
+
+    def load_model(self,)->CustomerSegmentationModel:
+        """
+        Load the model from the model_path
+        :return:
+        """
+
+        return self.s3.load_model(self.model_path,bucket_name=self.bucket_name)
+
+    def save_model(self,from_file,remove:bool=False)->None:
+        """
+        Save the model to the model_path
+        :param from_file: Your local system model path
+        :param remove: By default it is false that mean you will have your model locally available in your system folder
+        :return:
+        """
+        
+        try:
+            self.s3.upload_file(from_file,
+                                to_filename=self.model_path,
+                                bucket_name=self.bucket_name,
+                                remove=remove
+                                )
+        except Exception as e:
+            raise CustomerException(e, sys)
+
+
+    def predict(self,dataframe:DataFrame):
+        """
+        :param dataframe:
+        :return:
+        """
+        try:
+            print("=" * 80)
+            print("Prediction DataFrame")
+            print(dataframe)
+            print("=" * 80)
+
+            if self.loaded_model is None:
+                print("Loading model from S3...")
+                self.loaded_model = self.load_model()
+
+            print("Loaded Model:", type(self.loaded_model))
+
+            prediction = self.loaded_model.predict(dataframe)
+
+            print("Prediction:", prediction)
+
+            return prediction
+
+        except Exception as e:
+            raise CustomerException(e, sys)
